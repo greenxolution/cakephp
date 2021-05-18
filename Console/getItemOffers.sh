4d6a50cb3a7d8a43fd2670315e0a964e94fd1c68
Alan Hugo <alanhugo@MacBook-Pro-de-Alan.local>
2021-05-17 23:47:25 -0500

    add .sh files that execute in shell

---
+++ Console/getItemOffers.sh
@@ -0,0 +1,1 @@
+cake -app /var/www/html/greencloud/app Execute abc > /home/ec2-user/myfridaylove/logs/getItemOffers_$(date --utc +\%Y\%m\%d_\%H\%M\%S).log 2>&1
---
+++ Console/initMWSCatalog.sh
@@ -0,0 +1,1 @@
+cake -app /var/www/html/greencloud/app Execute initMWSCatalog > /home/ec2-user/myfridaylove/logs/initMWSCatalog_$(date --utc +\%Y\%m\%d_\%H\%M\%S).log 2>&1
