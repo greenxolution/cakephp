1cd6bad9cb60b04a853c586fdf497f9a95a96708
Alan Hugo <alanhugo@MacBook-Pro-de-Alan.local>
2021-05-19 20:50:22 -0500

    add fix of names in shell console

--- Console/getItemOffers.sh
+++ Console/getItemOffers.sh
@@ -1,1 +1,1 @@
-cake -app /var/www/html/greencloud/app Execute abc > /home/ec2-user/myfridaylove/logs/getItemOffers_$(date --utc +\%Y\%m\%d_\%H\%M\%S).log 2>&1
+cake -app /var/www/html/greencloud/app Execute getItemOffers > /home/ec2-user/myfridaylove/logs/getItemOffers_$(date --utc +\%Y\%m\%d_\%H\%M\%S).log 2>&1
--- Console/initEntrenue.sh
+++ Console/initEntrenue.sh
@@ -1,1 +1,1 @@
-cake -app /var/www/html/greencloud/app Execute initUploadEntrenue > /home/ec2-user/myfridaylove/logs/initUploadEntrenue_$(date --utc +\%Y\%m\%d_\%H\%M\%S).log 2>&1
+cake -app /var/www/html/greencloud/app Execute initEntrenue > /home/ec2-user/myfridaylove/logs/initEntrenue_$(date --utc +\%Y\%m\%d_\%H\%M\%S).log 2>&1
