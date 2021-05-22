71da1ed045873aa7fbe44458b6b7199c92c642d8
Alan Hugo <alanhugo@MacBook-Pro-de-Alan.local>
2021-05-22 00:43:44 -0500

    add change var cake in sh

--- Console/initEntrenue.sh
+++ Console/initEntrenue.sh
@@ -1,1 +1,1 @@
-cake -app /var/www/html/greencloud/app Execute initUploadEntrenue > /home/ec2-user/myfridaylove/logs/initEntrenue_$(date --utc +\%Y\%m\%d_\%H\%M\%S).log 2>&1
+/var/www/html/greencloud/app/Console/cake -app /var/www/html/greencloud/app Execute initUploadEntrenue > /home/ec2-user/myfridaylove/logs/initEntrenue_$(date --utc +\%Y\%m\%d_\%H\%M\%S).log 2>&1
